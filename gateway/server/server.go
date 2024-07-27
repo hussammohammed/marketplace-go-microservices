82cc3b6c5eea0fd0ed141e133e48c9185c92a51d
Hossam <hossammdpro@dev.azure.com>
2024-07-27 17:30:24 +0300

    client grpc client for user service at api gateway

--- gateway/server/server.go
+++ gateway/server/server.go
@@ -1,15 +1,25 @@
 package server
 
 import (
+	"flag"
 	"fmt"
+	"log"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	userService "github.com/hussammohammed/marketplace-go-microservices/gateway/server/grpcClients/protos/user"
 	routes "github.com/hussammohammed/marketplace-go-microservices/gateway/server/routes"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	userSvcAddr = flag.String("userSvcAddr", "localhost:50051", "the address to connect to")
 )
 
 func Run() error {
+	flag.Parse()
 	// create gin router with default middleware
 	router := gin.Default()
 	// CORS config
@@ -18,8 +28,17 @@ func Run() error {
 	config.AllowCredentials = true
 	config.AllowHeaders = []string{"Origin", "Authorization", "Content-Type", "Access-Control-Allow-Origin"}
 	router.Use(cors.New(config))
-
-	routes.DebuggingRoutes(router)
 	routes.AuthRoutes(router)
+	// initialize grpc services
+	// initialize user service
+	userSvcConn, err := grpc.Dial(*userSvcAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Printf("faild to connect to user service: %v", err)
+	} else {
+		defer userSvcConn.Close()
+		userSvcClient := userService.NewUserClient(userSvcConn)
+		routes.DebuggingRoutes(router, userSvcClient)
+	}
+
 	return router.Run(fmt.Sprintf("%v:%v", viper.GetString("server.host"), viper.GetString("server.port")))
 }
